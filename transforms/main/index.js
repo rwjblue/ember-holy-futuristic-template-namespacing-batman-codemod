84d2003cb302609732919c359bd2ef1d60693a92
Robert Jackson <rjackson@linkedin.com>
2019-08-27 18:15:09 -0400

    Add basic implementation for codemod.

--- transforms/main/index.js
+++ transforms/main/index.js
@@ -1,15 +1,57 @@
-const { getParser } = require('codemod-cli').jscodeshift;
-const { getOptions } = require('codemod-cli');
-const templateRecast = require('ember-template-recast');
 const { transform } = require('ember-template-recast');
 
-module.exports = function transformer(file, api) {
+module.exports = function transformer(file) {
   let { code } = transform(file.source, env => {
     let { builders: b } = env.syntax;
 
+    function rewriteOrWrapComponentParam(node, b) {
+      if (!node.params.length) {
+        return;
+      }
+
+      let firstParam = node.params[0];
+      if (firstParam.type === 'StringLiteral') {
+        node.params[0] = b.string(firstParam.original.replace('::', '$'));
+        return;
+      }
+    }
+
     return {
-      MustacheStatement() {
-        return b.mustache(b.path('wat-wat'));
+      PathExpression(node) {
+        if (node.parts.length > 1 || !node.original.includes('::')) {
+          return;
+        }
+
+        return b.path(node.original.replace('::', '$'));
+      },
+      ElementNode(node) {
+        if (node.tag.indexOf('::') > -1) {
+          node.tag = node.tag.replace('::', '$');
+        }
+      },
+      MustacheStatement(node) {
+        if (node.path.original !== 'component') {
+          // we don't care about non-component expressions
+          return;
+        }
+
+        rewriteOrWrapComponentParam(node, b);
+      },
+      SubExpression(node) {
+        if (node.path.original !== 'component') {
+          // we don't care about non-component expressions
+          return;
+        }
+
+        rewriteOrWrapComponentParam(node, b);
+      },
+      BlockStatement(node) {
+        if (node.path.original !== 'component') {
+          // we don't care about blocks not using component
+          return;
+        }
+
+        rewriteOrWrapComponentParam(node, b);
       },
     };
   });
